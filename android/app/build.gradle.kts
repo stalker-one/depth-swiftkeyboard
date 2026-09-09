1cfdd08ba184fb3aa56beb8685d8aba34ce0a5c3
stalker-one <stalkermianone10@gmail.com>
2026-09-09 15:28:37 +0500

    chore(android): bump app version to 1.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,8 +3,17 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-android { namespace = "com.stalkerone.depthkeyboard"; compileSdk = 35
-    defaultConfig { applicationId = "com.stalkerone.depthkeyboard"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+android {
+    namespace = "com.stalkerone.depthkeyboard"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.stalkerone.depthkeyboard"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.1.0"
+    }
 }
 
 kotlin { jvmToolchain(17) }
